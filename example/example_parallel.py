591bac92720138d17838d45c2635b4b6b30e965d
AnthonyMRios <anthonymrios@gmail.com>
2016-12-09 14:28:20 -0500

    updated examples

--- example/example_parallel.py
+++ example/example_parallel.py
@@ -18,32 +18,32 @@ def main():
     labels_test = joblib.load('./test_data/bibtex-Y-test.pkl')
     print X.shape, labels.shape, X.getformat(), labels.getformat()
 
-    print 'training LEML'
+    print 'Training LEML'
     sys.stdout.flush()
     #for l in [1e-3, 1e-2, 1e-1, 1., 10]:
     t0 = time()
     leml = LEML.get_instance('parallel', num_factors=200, num_iterations=25, reg_param=1., verbose=True)
     leml.fit(X.tocsc(), labels.tocsc())
-    print 'Train time',time() - t0
+    print 'Train time', time() - t0, 'seconds'
     sys.stdout.flush()
     preds = leml.predict(X_test)
     preds_top_k = preds.argsort()[:,::-1]
     preds_top_k = preds_top_k[:,:1]
     new_preds = np.zeros((preds.shape[0], preds.shape[1]))
     new_preds[np.arange(preds.shape[0]).repeat(1),preds_top_k.flatten()] = 1
-    print precision_score(labels_test.toarray(), new_preds, average='samples')
+    print 'Precision @ 1:', precision_score(labels_test.toarray(), new_preds, average='samples')
 
     preds_top_k = preds.argsort()[:,::-1]
     preds_top_k = preds_top_k[:,:3]
     new_preds = np.zeros((preds.shape[0], preds.shape[1]))
     new_preds[np.arange(preds.shape[0]).repeat(3),preds_top_k.flatten()] = 1
-    print precision_score(labels_test.toarray(), new_preds, average='samples')
+    print 'Precision @ 3:', precision_score(labels_test.toarray(), new_preds, average='samples')
 
     preds_top_k = preds.argsort()[:,::-1]
     preds_top_k = preds_top_k[:,:5]
     new_preds = np.zeros((preds.shape[0], preds.shape[1]))
     new_preds[np.arange(preds.shape[0]).repeat(5),preds_top_k.flatten()] = 1
-    print precision_score(labels_test.toarray(), new_preds, average='samples')
+    print 'Precision @ 5:', precision_score(labels_test.toarray(), new_preds, average='samples')
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -1,13 +1,12 @@
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
+    with open('README.md') as f:
         return f.read()
 
 setup(name='leml',
       version='0.1',
       description='Python version of the LEML algorithm',
-      long_description=readme(),
       url='https://github.com/AnthonyMRios/pyleml',
       author='Anthony Rios',
       author_email='anthonymrios@gmail.com',
